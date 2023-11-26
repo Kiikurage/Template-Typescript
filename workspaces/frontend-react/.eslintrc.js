ecb086f5818469fcd25434f018b4481b974584c8
Yuichiro Kikura <kikurage@YuichironoMacBook-Pro.local>
2023-11-26 11:04:51 +0900

    Update

--- workspaces/frontend-react/.eslintrc.js
+++ workspaces/frontend-react/.eslintrc.js
@@ -1,3 +1,13 @@
 module.exports = {
-    extends: ['../../.eslintrc.js'],
+    overrides: [
+        {
+            files: ['**/*.jsx', '**/*.tsx'],
+            extends: ['plugin:react/recommended', 'plugin:react-hooks/recommended'],
+            plugins: ['react', 'react-hooks'],
+            rules: {
+                'react-hooks/exhaustive-deps': 'error',
+                'react/react-in-jsx-scope': 'off',
+            },
+        },
+    ],
 };
